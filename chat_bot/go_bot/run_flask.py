2f1c67817a060934549dce4c3fc1996116479ebc
Karpov Nikolay <karpnv@gmail.com>
2018-09-09 00:06:03 +0300

    add run_flask

---
+++ chat_bot/go_bot/run_flask.py
@@ -0,0 +1,21 @@
+from flask import request
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/hi')
+def first_page():
+    content="BotaNick"
+    html="""<head></head><body>%s</body>""" %content
+    return html
+
+@app.route('/', methods=['POST'])
+def post_request():
+    context = request.get_json(force=True)
+    resp="""["%s"]""" %context
+    return resp
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',
+            port=8081,
+            debug=True)
